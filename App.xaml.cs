cb7b3775b910a754d15e3b695293a2f1afacf225
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-02-17 08:04:08 +0100

    providing App class

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WPF_CRUD.Data;
 
 namespace WPF_CRUD
 {
@@ -14,7 +17,28 @@ namespace WPF_CRUD
     public partial class App : Application
     {
 
-        
+        private IServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection service = new ServiceCollection();
+            service.AddDbContext<CzytelnikDB>(option =>
+            {
+                option.UseSqlite("Data Source = Biblioteka.db");
+            }
+            );
+
+            service.AddSingleton<MainWindow>();
+
+            serviceProvider = service.BuildServiceProvider();
+        }
+
+
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
 
     }
 }
