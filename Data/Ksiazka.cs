f95649a7959abca44e795bec31fd29df0d6a1aa6
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-02-20 10:32:20 +0100

    adding Ksiazka and KsiazkaDB classes

---
+++ Data/Ksiazka.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_CRUD.Data
+{
+    public class Ksiazka
+    {
+        public int ID { get; set; }
+        public string gatunek { get; set; }
+        public string tytul { get; set; }
+    }
+}
---
+++ Data/KsiazkaDB.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_CRUD.Data
+{
+    class KsiazkaDB : DbContext
+    {
+
+        public KsiazkaDB(DbContextOptions<KsiazkaDB> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Ksiazka> Ksiazka { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ksiazka>().HasData(GetKsiazka());
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        private Ksiazka[] GetKsiazka()
+        {
+            return new Ksiazka[]
+            {
+                new Ksiazka { ID =1, gatunek ="sci-fi", tytul ="Solaris"},
+
+            };
+        }
+
+    }
+}
